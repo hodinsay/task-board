e43a65d0012bea03c96885e8e3d1343732dd5abd
Din Ho <hodin81@gmail.com>
2023-04-01 00:07:08 -0700

    Pomodoro WIP

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,17 +1,55 @@
+import { useState, useEffect } from 'react';
 import './Timer.scss';
+import Control from './Control/Control';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const Timer = () => {
+  const [buttonText, setButtonText] = useState('START');
+  const [timerMode, setTimerMode] = useState(25);
+  // const [pomoLength, setPomoLength] = useState(25);
+  // const [shortLength, setShortLength] = useState(5);
+  // const [longLength, setLongLength] = useState(15);
+  const [isActive, setIsActive] = useState(false);
+  // const [secondsLeft, setSecondsLeft] = useState(pomoLength * 60);
+
+  useEffect(() =>{
+    if(isActive){
+      const interval = setInterval(() => {
+        setTimerMode(timerMode => timerMode - 1)
+      }, 1000);
+
+      if(timerMode === 0){
+        clearInterval(interval);
+        setIsActive(false);
+        alert('Time Up!')
+      };
+
+      return () => clearInterval(interval);
+    }
+
+  }, [isActive, timerMode])
+
+  const buttonHandler = () => {
+    if (buttonText === 'START') {
+      setIsActive(true)
+      setButtonText('PAUSE');
+    }
+    if (buttonText === 'PAUSE') {
+      setIsActive(false)
+      setButtonText('START');
+    }
+  };
+
   return (
     <Card className='timer'>
-        <div>
-            <a href="#">Pomodoro</a>
-            <a href="#">Short Break</a>
-            <a href="#">Long Break</a>
-        </div>
-        <span>25:00</span>
-        <Button className='timer-btn'>Start / Pause</Button>
+        <Control 
+        setIsActive={setIsActive}
+        setTimerMode={setTimerMode}
+        setButtonText={setButtonText}
+        />
+        <span>{timerMode}</span>
+        <Button className='timer-btn' onClick={buttonHandler}>{buttonText}</Button>
     </Card>
   );
 };
